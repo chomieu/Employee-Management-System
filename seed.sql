d51dab05112aa6eaa1764ed2bb834174880bf296
chomieu <chomieu@gmail.com>
2021-01-24 21:41:41 -0800

    Included seed.sql

---
+++ seed.sql
@@ -0,0 +1,21 @@
+USE employeeDB;
+
+INSERT INTO department (department)
+VALUES 
+("Executive"),
+("IT"),
+("Human Resources");
+
+INSERT INTO `role` (title, salary, department_id)
+VALUES 
+("CEO", "500000", "1"),
+("CTO", "250000", "2"),
+("Engineer", "100000", "2"),
+("Recruiter", "70000", "3");
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES 
+("Chomie", "Usaneerungrueng", "1"),
+("Will", "Nguyen", "2"),
+("Isaac", "Tian", "3"),
+("Irene", "Wachirawutthichai", "3");
